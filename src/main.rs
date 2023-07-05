eee92996164fb4622cd395237c6254d8161680a4
Isaac Grosof <isaacbg227@gmail.com>
2023-07-05 18:19:51 -0400

    Working version

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use noisy_float::prelude::*;
 use rand::prelude::*;
 use rand_distr::Exp;
 
+const EPSILON: f64 = 1e-8;
+const INFINITY: f64 = f64::INFINITY;
+
 struct Job {
     arrival_time: f64,
     original_size: f64,
@@ -9,19 +12,39 @@ struct Job {
     id: u64,
 }
 
+#[derive(Debug)]
 enum Dist {
-    Exp(f64),
+    Exp,
     Discrete(Vec<(f64, f64)>),
 }
 impl Dist {
     fn sample<R: Rng>(&self, rng: &mut R) -> f64 {
-        todo!()
+        match self {
+            Dist::Exp => {
+                let exp = Exp::new(1.0).expect("valid");
+                exp.sample(rng)
+            }
+            Dist::Discrete(cases) => {
+                let mut num: f64 = rng.gen();
+                for (prob, value) in cases {
+                    num -= prob;
+                    if num <= 0.0 {
+                        return *value;
+                    }
+                }
+                unreachable!("{:?}", cases)
+            }
+        }
     }
     fn mean_size(&self) -> f64 {
-        todo!()
+        match self {
+            Dist::Exp => 1.0,
+            Dist::Discrete(cases) => cases.iter().map(|(prob, value)| prob * value).sum(),
+        }
     }
 }
 
+#[derive(Debug)]
 enum Policy {
     Global_NP_FCFS,
     Local_FCFS,
@@ -31,8 +54,51 @@ enum Policy {
 }
 impl Policy {
     // Set of indices to share amongst
-    fn serve(&self, queue: &[Job]) -> Vec<usize> {
-        todo!()
+    fn serve(&self, queue: &[Job], num_servers: usize) -> Vec<usize> {
+        match self {
+            Policy::Global_NP_FCFS => {
+                let mut indices: Vec<usize> = (0..queue.len()).collect();
+                if indices.len() <= num_servers {
+                    return indices;
+                }
+                indices.sort_by_key(|i| {
+                    let job = &queue[*i];
+                    let key = if job.original_size > job.remaining_size {
+                        -1.0
+                    } else {
+                        job.arrival_time
+                    };
+                    n64(key)
+                });
+                indices[..num_servers].to_vec()
+            }
+            Policy::Local_FCFS => (0..queue.len()).take(num_servers).collect(),
+            Policy::Global_P_FCFS => {
+                let mut indices: Vec<usize> = (0..queue.len()).collect();
+                if indices.len() <= num_servers {
+                    return indices;
+                }
+                indices.sort_by_key(|i| {
+                    let job = &queue[*i];
+                    let key = job.arrival_time;
+                    n64(key)
+                });
+                indices[..num_servers].to_vec()
+            }
+            Policy::Global_LPS(jobs_in_service) => {
+                let mut indices: Vec<usize> = (0..queue.len()).collect();
+                if indices.len() <= *jobs_in_service {
+                    return indices;
+                }
+                indices.sort_by_key(|i| {
+                    let job = &queue[*i];
+                    let key = job.arrival_time;
+                    n64(key)
+                });
+                indices[..*jobs_in_service].to_vec()
+            }
+            Policy::PS => (0..queue.len()).collect(),
+        }
     }
 }
 
@@ -42,10 +108,32 @@ struct Results {
 }
 impl Results {
     fn new(bucket_width: f64) -> Self {
-        todo!()
+        Results {
+            bucket_width,
+            bucket_counts: vec![],
+        }
     }
     fn add_response(&mut self, response: f64) {
-        todo!()
+        let target_bucket = (response / self.bucket_width).floor() as usize;
+        if self.bucket_counts.len() <= target_bucket {
+            self.bucket_counts
+                .extend((self.bucket_counts.len()..=target_bucket).map(|_| 0));
+        }
+        self.bucket_counts[target_bucket] += 1;
+    }
+    fn percentile(&self, perc: f64) -> f64 {
+        assert!(perc >= 0.0);
+        assert!(perc <= 1.0);
+        let total: u64 = self.bucket_counts.iter().sum();
+        let target = (total as f64 * perc) as u64;
+        let mut running_count = 0;
+        for (i, count) in self.bucket_counts.iter().enumerate() {
+            running_count += count;
+            if running_count >= target {
+                return i as f64 * self.bucket_width;
+            }
+        }
+        unreachable!("Perc: {}", perc);
     }
 }
 
@@ -56,26 +144,143 @@ fn simulate(
     num_stages: usize,
     rho: f64,
     num_jobs: u64,
+    bucket_width: f64,
     seed: u64,
 ) -> Results {
-    todo!()
+    assert!((dist.mean_size() - 1.0).abs() < EPSILON);
+    let arrival_rate = rho;
+    let arrival_dist = Exp::new(arrival_rate).expect("Valid rate");
+    let mut rng = StdRng::seed_from_u64(seed);
+    let mut num_completions = 0;
+    let mut num_arrivals = 0;
+    let mut time = 0.0;
+    // Stored in LocalFCFS order
+    let mut queues: Vec<Vec<Job>> = (0..num_stages).map(|_| vec![]).collect();
+    let mut results = Results::new(bucket_width);
+    let mut to_append = vec![];
+    while num_completions < num_jobs {
+        let mut next_comp_diff = INFINITY;
+        let mut services = vec![];
+        for queue in &queues {
+            let mut service = policy.serve(&queue, num_servers_per_stage);
+            service.sort();
+            assert!(service.len() >= num_servers_per_stage.min(queue.len()));
+            for &s in &service {
+                // Each job runs at speed inversely proportional
+                // to the number of jobs in service if >= num_servers in service,
+                // otherwise at speed 1/num_servers
+                let time_to_completion =
+                    queue[s].remaining_size * service.len().max(num_servers_per_stage) as f64;
+                next_comp_diff = next_comp_diff.min(time_to_completion);
+            }
+            services.push(service);
+        }
+        // Valid to resample because Poisson
+        let arrival_diff = arrival_dist.sample(&mut rng);
+        let is_arrival = arrival_diff < next_comp_diff;
+        let event_diff = arrival_diff.min(next_comp_diff);
+        time += event_diff;
+        for stage in 0..num_stages {
+            let queue = &mut queues[stage];
+            let service = &services[stage];
+            // Reverse sorted order, safe to delete
+            for &s in service.iter().rev() {
+                // Each job runs at speed inversely proportional
+                // to the number of jobs in service if >= num_servers in service,
+                // otherwise at speed 1/num_servers
+                queue[s].remaining_size -=
+                    event_diff / service.len().max(num_servers_per_stage) as f64;
+                if queue[s].remaining_size < EPSILON {
+                    let job = queue.remove(s);
+                    if stage == num_stages - 1 {
+                        num_completions += 1;
+                        results.add_response(time - job.arrival_time);
+                    } else {
+                        let new_size = dist.sample(&mut rng);
+                        let new_job = Job {
+                            arrival_time: job.arrival_time,
+                            remaining_size: new_size,
+                            original_size: new_size,
+                            id: job.id,
+                        };
+                        to_append.push(new_job);
+                    }
+                }
+            }
+            if stage < num_stages - 1 {
+                queues[stage + 1].append(&mut to_append);
+            }
+            assert!(to_append.is_empty());
+        }
+        if is_arrival {
+            let new_size = dist.sample(&mut rng);
+            let new_job = Job {
+                arrival_time: time,
+                remaining_size: new_size,
+                original_size: new_size,
+                id: num_arrivals,
+            };
+            queues[0].push(new_job);
+            num_arrivals += 1;
+        }
+    }
+    results
 }
 fn main() {
     let num_servers_per_stage = 3;
     let num_stages = 3;
-    let policy = Policy::Global_LPS(6);
-    let dist = Dist::Exp(1.0);
-    let rho = 0.5;
+    let policies = vec![
+        Policy::Global_NP_FCFS,
+        Policy::Global_P_FCFS,
+        Policy::Global_LPS(6),
+        Policy::Local_FCFS,
+        Policy::PS,
+    ];
+    let dist_num = 1;
+    let dist = match dist_num {
+        0 => Dist::Exp,
+        1 => {
+            let p = 1.0 / 40.0;
+            Dist::Discrete(vec![
+                (p, 1.0 / (2.0 * p)),
+                (1.0 - p, 1.0 / (2.0 * (1.0 - p))),
+            ])
+        }
+        _ => unimplemented!()
+    };
+    let rhos = vec![
+        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95,
+    ];
+    //let rhos = vec![0.96, 0.97, 0.98, 0.99];
     let num_jobs = 1_000_000;
     let seed = 0;
-    let results = simulate(
-        &policy,
-        &dist,
-        num_servers_per_stage,
-        num_stages,
-        rho,
-        num_jobs,
-        seed,
+    let bucket_width = 0.1;
+    let perc = 0.99;
+    println!(
+        "servers {}; stages {}; dist {:?}; jobs {}; bucket {}; seed {}",
+        num_servers_per_stage, num_stages, dist, num_jobs, bucket_width, seed
     );
-    todo!()
+    println!("{}th percentile of response time", perc);
+    print!("rho;");
+    for policy in &policies {
+        print!("{:?};", policy);
+    }
+    println!();
+    for rho in rhos {
+        print!("{};", rho);
+        for policy in &policies {
+            let results = simulate(
+                policy,
+                &dist,
+                num_servers_per_stage,
+                num_stages,
+                rho,
+                num_jobs,
+                bucket_width,
+                seed,
+            );
+            print!("{};", results.percentile(perc));
+        }
+        println!();
+    }
 }
